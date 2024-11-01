0422fc7ac3f7c1eb28ab88cad00ccc50665e8d28
DimasAditya2 <esadimasadityaa@gmail.com>
2024-11-01 18:40:27 +0700

    Basic Component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
-<script setup></script>
+<script setup>
+  import MainButton from "./components/MainButton.vue";
+  const greeting = 'Hello World!'
+</script>
 
 <template>
-  <h1>Hello World!</h1>
+  <h1>{{ greeting }}</h1>
+  <p>Esa Dimas Aditya</p>
+  <MainButton/>
 </template>
 
 <style scoped></style>
---
+++ src/components/MainButton.vue
@@ -0,0 +1,11 @@
+<script setup>
+  defineOptions({
+    name: 'mainButton'
+  })
+</script>
+
+<template>
+  <button>Click me!</button>
+</template>
+
+<style scoped></style>
