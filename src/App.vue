2f80bcd4513502ad1bd55efbb1175a995a8bc8ff
DimasAditya2 <esadimasadityaa@gmail.com>
2024-11-12 23:20:38 +0700

    Component Registration

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <script setup>
-  import MainButton from "./components/MainButton.vue";
-  import OptionComponents from './components/OptionsComponent.vue'
-  import CompositionComponent from './components/CompositionComponent.vue'
   const greeting = 'Hello World!'
 </script>
 
 <template>
   <h1>{{ greeting }}</h1>
-  <OptionComponents/>
+  <OptionComponent/>
   <CompositionComponent/>
   <MainButton/>
 
--- src/main.js
+++ src/main.js
@@ -2,5 +2,29 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import CompositionComponent from './components/CompositionComponent.vue'
+import OptionComponent from './components/OptionComponent.vue'
+import MainButton from './components/MainButton.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+// ? Global component
+
+// todo: notes
+//  use if component reusable
+//  use PascalCase Names
+
+// can chaining
+// app
+//   .component('CompositionComponent', CompositionComponent)
+//   .component('OptionComponent', OptionComponent)
+//   .component('MainButton', MainButton)
+
+app.component("CompositionComponent", CompositionComponent)
+app.component("OptionComponent", OptionComponent)
+app.component("MainButton", MainButton)
+
+
+app.mount('#app')
+
+// createApp(App).mount('#app')
