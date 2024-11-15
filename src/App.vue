6053632cb8f03e096b8538d0384a04804b44a233
DimasAditya2 <esadimasadityaa@gmail.com>
2024-11-15 18:18:44 +0700

    Interpolation and Bindings

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <script setup>
   const greeting = 'Hello World!'
+  const blogPost = '<p style="color: red;">Ini Adalah BlogPost</p>'
+  const idBlog = 1
+  const blogClass = 'text-4xl flex flex-col justify-center'
 </script>
 
 <template>
@@ -8,6 +11,14 @@
   <CompositionComponent/>
   <MainButton/>
 
+  <!-- Attribute Bindings -->
+   <!-- The v-bind directive instructs Vue to keep the element's id attribute in sync with the component's dynamicId property. If the bound value is null or undefined, then the attribute will be removed from the rendered element. -->
+  <div :id="idBlog" :class="blogClass" >
+    {{ blogPost }}
+  </div>
+  <div v-html="blogPost">
+
+  </div>
 </template>
 
 <style scoped></style>
--- src/components/MainButton.vue
+++ src/components/MainButton.vue
@@ -2,10 +2,26 @@
   defineOptions({
     name: 'mainButton'
   })
+  const buttonAttr = {
+    disabled: isLoading,
+    class: 'text-center',
+    id: 1
+  }
+
+  const renderIsLoading = () => {
+    if (isLoading) {
+      return 'loading'
+    } else {
+      return 'submit'
+    }
+  }
+
+  const isLoading = true
+
 </script>
 
 <template>
-  <button>Click me!</button>
+  <button v-bind="buttonAttr" :id="`id-${buttonAttr.class}`">{{ renderIsLoading() }}</button>
 </template>
 
 <style scoped></style>
