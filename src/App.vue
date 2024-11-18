2ebd2e77d234c17e61286cb329999b6ef28c5537
DimasAditya2 <esadimasadityaa@gmail.com>
2024-11-18 21:36:33 +0700

    Reactivity State

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <script setup>
+import { reactive, ref } from 'vue'
+
 const greeting = 'Hello World!'
 const blogPost = '<p style="color: red;">Ini Adalah BlogPost</p>'
 const idBlog = 1
 const blogClass = 'text-4xl flex flex-col justify-center'
 const submitButton = {
   title: 'Submit',
-  disabled: false
+  disabled: false,
 }
+
+const count = ref(1)
+
+const increment = () => {
+  count.value++
+}
+
+const user = reactive(['dimas', 'dwi'])
 </script>
 
 <template>
@@ -16,9 +26,9 @@ const submitButton = {
 
   <MainButton :title="`Button`" />
   <MainButton :title="`Submit`" />
-  <MainButton v-bind="submitButton"/>
+  <MainButton v-bind="submitButton" />
   <!-- special casting -->
-  <MainButton disabled/>
+  <MainButton disabled />
 
   <!-- Attribute Bindings -->
   <!-- The v-bind directive instructs Vue to keep the element's id attribute in sync with the component's dynamicId property. If the bound value is null or undefined, then the attribute will be removed from the rendered element. -->
@@ -33,6 +43,13 @@ const submitButton = {
 
   <!-- props -->
   <BlogPost title="My journey with Vue" />
+
+  <button @click="increment">{{ count }}</button>
+
+  <div>
+    <h1>name: {{ user[0] }}</h1>
+    <button @click="user.push('fifi')">Tambah user {{ user[2] }}</button>
+  </div>
 </template>
 
 <style scoped></style>
