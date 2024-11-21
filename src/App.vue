23214e40d1588fe3a95f4d2e34d51e105e12720c
DimasAditya2 <esadimasadityaa@gmail.com>
2024-11-21 23:31:36 +0700

    Event Handling

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <script setup>
-import { ref } from 'vue';
-import MainButton from './components/MainButton.vue';
-const disabled = ref(false)
-
-const isDisabled = () => {
-  disabled.value = !disabled.value
-}
-
-
+import EventHandling from './components/EventHandling.vue'
 </script>
 
 <template>
-  <MainButton @click="isDisabled" :disabled="disabled" class="halo"/>
+  <EventHandling />
 </template>
 
 <style scoped></style>
---
+++ src/components/EventHandling.vue
@@ -0,0 +1,44 @@
+<script>
+  export default {
+    data() {
+      return {
+        count: 0,
+        name: 'Vue.js'
+      }
+    },
+    methods: {
+      handleClick() {
+        alert('Tombol diklik')
+      },
+      sayHello(name) {
+        alert(`Halo, ${name}`)
+      },
+      greet(event) {
+        alert(`Hello ${this.name}`)
+
+        if (event) {
+          alert(event.target.tagName)
+        }
+      }
+    }
+  }
+</script>
+
+<template>
+  <div>
+    <button v-on:click="handleClick">Klik saya!</button>
+    <!-- or -->
+     <button @click="handleClick">Klik saya!</button>
+
+     <button @click="() => console.log('hello world')">console.log()</button>
+
+     <button @click="sayHello('dimas')">Sapa saya</button>
+
+     <button @click="count++">Add 1</button>
+     <p>Count is: {{ count }}</p>
+
+     <hr>
+
+     <button @click="greet">Greet</button>
+  </div>
+</template>
